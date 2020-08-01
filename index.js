6ebab4a5d42966af2c253b58f51af73971bc7ab1
mizuho1998 <moc.liam0000@gmail.com>
2020-08-01 16:48:25 +0900

    Changes to be committed:        modified:   index.html        modified:   index.js

--- index.js
+++ index.js
@@ -1,3 +1,47 @@
-(function(){
+(async function(){
     console.log("hello treasure");
+    let localStream;
+
+    try {
+        localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+        const videoElement = document.getElementById('my-video');
+        console.log(localStream);
+        console.log(videoElement);
+        videoElement.srcObject = localStream;
+        videoElement.play();
+    } catch(error) {
+        alert(error);
+    }
+
+    const peer = new Peer({
+        key: 'a40fde66-0ac5-4525-87b8-9f77b16b5bbd',
+        debug: 3
+    });
+
+    peer.on('open', () => {
+        document.getElementById('my-id').textContent = peer.id;
+    });
+
+    // 発信処理
+    document.getElementById('make-call').onclick = () => {
+        const theirID = document.getElementById('their-id').value;
+        const mediaConnection = peer.call(theirID, localStream);
+        setEventListener(mediaConnection);
+    };
+    
+    // イベントリスナを設置する関数
+    const setEventListener = mediaConnection => {
+        mediaConnection.on('stream', stream => {
+        // video要素にカメラ映像をセットして再生
+        const videoElm = document.getElementById('their-video')
+        videoElm.srcObject = stream;
+        videoElm.play();
+        });
+    }
+
+    //着信処理
+    peer.on('call', mediaConnection => {
+        mediaConnection.answer(localStream);
+        setEventListener(mediaConnection);
+    });
 })();
